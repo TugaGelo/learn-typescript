4daedc5385b85e282313cc4578308a64778a14f7
Tugaga <rolandtugaoen013@gmail.com>
2025-05-16 13:50:19 +0800

    feat: Implemented examples of advanced TypeScript types

--- src/index.ts
+++ src/index.ts
@@ -35,15 +35,77 @@ function calculateTax(income: number, taxYear = 2022): number{
 
 calculateTax(10_000);
 
-//Object
-let employee: {
+//Object 
+//Type Alias
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = { 
+}
+
+let employee: Employee = {
     id: 1, 
     name: 'Mosh',
     retire: (date: Date) => {
         console.log(date)
     }
 };
+
+//Union
+function kgToLbs(weight: number | string){
+    //Narrowing
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
+
+//Intersection
+type Draggable = { 
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+//Literal
+type Quantity = 50 | 100;
+let quantity: Quantity = 50;
+
+type Metric = "cm" | "inch";
+
+//Nullable
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
+}
+
+greet(null);
+
+//Optional Chaining
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date()};
+}
+
+let customer = getCustomer(0);
+console.log(customer?.birthday?.getFullYear());
+
+let log: any = null;
+log?.('a');
